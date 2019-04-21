581090d571caf3b01d222a962d404d06bbe91d46
xchen-hmc <xchen@cs.hmc.edu>
2019-04-21 16:09:48 -0700

    good rn

--- source/weeklyDroughtMap.py
+++ source/weeklyDroughtMap.py
@@ -1,5 +1,5 @@
 import plotly
-plotly.tools.set_credentials_file(username='cle', api_key='FGWVL1Oa1la0wPprPRbL')
+# plotly.tools.set_credentials_file(username='cle', api_key='FGWVL1Oa1la0wPprPRbL')
 
 import plotly.plotly as py
 import plotly.figure_factory as ff
@@ -9,25 +9,32 @@ import os
 import time
 
 # Recurses through Drought Level Folder to read each csv file in folder
-for csv_file in os.listdir("../data/Weekly Drought Level Graphs"):
+
+for csv_file in sorted(os.listdir("../data/Weekly Drought Level Graphs")):
+    # print(csv_file)
+    date = csv_file.split("_")[-1]
+    date = date.split(".")[0]
     csv_file="../data/Weekly Drought Level Graphs/"+csv_file
-    print(csv_file)
+    # date = date.replace("-", " ")
     df_sample = pd.read_csv(csv_file)
     values = df_sample['level_d'].tolist() # Read in values contained within your file
     fips = df_sample['fips'].tolist() # Read in FIPS Codes
-    colorscale = ["#171c42","#1267b2","#8cb5c9","#dab2be","#c46852","#701b20"] # Create a colorscale
-
+    colorscale = ["#ffffe5", "#feeba2","#febc46","#f07818","#b74203","#662506"] # Create a colorscale
+    # "#ffffe5"
     # For colorscale help: https://react-colorscales.getforge.io/
-    time.sleep(50)
 
-    endpts = list(np.linspace(0, 4, len(colorscale) - 1)) # Identify a suitable range for your data
+    # endpts = list(np.linspace(0, 4, len(colorscale) - 1)) # Identify a suitable range for your data
 
     fig = ff.create_choropleth(
         fips=fips, values=values, colorscale=colorscale, show_state_data=True, # If your values is a list of numbers, you can bin your values into half-open intervals
         county_outline={'color': 'rgb(255,255,255)', 'width': 0.5}, 
-        legend_title='drought level', title='Drought level for week of'
+        legend_title='drought level', title='Drought level for week of '+ str(date)
     )
-    py.iplot(fig, filename = 'Weekly Drought Level for week of')
+    curr_name = 'DroughtLevel_wk_'+ str(date)
+    
+    py.plot(fig, filename = curr_name)
+    # time.sleep(0.5)
+    print('Done with week ' + date)
 
 
 # def createLevels(final_drought_dt = df_sample):
